40c98008195571a27fe5491ea08e08b769db817a
Gehrer Gerhard <g.gehrer@htl-leonding.ac.at>
2025-06-11 00:01:58 +0200

    Fixed loadUserFromStorage().

--- SETemplate.AngularApp/src/app/services/auth.service.ts
+++ SETemplate.AngularApp/src/app/services/auth.service.ts
@@ -176,17 +176,17 @@ export class AuthService {
   private async loadUserFromStorage() {
     this._user = this.storageService.getData(StorageLiterals.USER) as IAuthenticatedUser;
 
-    if (this._user != null) {
-      try {
-        const sessionAlive = await this.accountService.isSessionAlive(this._user.sessionToken);
-        if (!sessionAlive) {
-          this.resetUser();
-        }
-      } catch (error) {
-        console.error('Fehler beim Überprüfen der Session:', error);
-        this.resetUser();
-      }
-    }
+    //if (this._user != null) {
+    //  try {
+    //    const sessionAlive = await this.accountService.isSessionAlive(this._user.sessionToken);
+    //    if (!sessionAlive) {
+    //      this.resetUser();
+    //    }
+    //  } catch (error) {
+    //    console.error('Fehler beim Überprüfen der Session:', error);
+    //    this.resetUser();
+    //  }
+    //}
     this.notifyForUserChanged();
   }
 
