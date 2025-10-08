f79fdc7198f2634fdf82a63775c57fc042e519ef
Gehrer Gerhard <g.gehrer@htl-leonding.ac.at>
2025-10-08 13:20:18 +0200

    Fixed angular.

---
+++ SETemplate.AngularApp/src/app/components/entities/version-entity-object-edit.component.ts
@@ -0,0 +1,18 @@
+﻿//@CustomCode
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
+import { VersionEntityObjectBaseEditComponent }from '@app/components/entities/version-entity-object-base-edit.component';
+//@CustomImportBegin
+//@CustomImportEnd
+@Component({
+  selector:'app-version-entity-object-edit',
+  imports: [ CommonModule, FormsModule, TranslateModule],
+  templateUrl: './version-entity-object-edit.component.html',
+  styleUrl: './version-entity-object-edit.component.css'
+})
+export class VersionEntityObjectEditComponent extends VersionEntityObjectBaseEditComponent {
+//@CustomCodeBegin
+//@CustomCodeEnd
+}
---
+++ SETemplate.AngularApp/src/app/pages/entities/version-entity-object-list.component.ts
@@ -0,0 +1,41 @@
+﻿//@CustomCode
+import { IdType, IdDefault } from '@app/models/i-key-model';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { IVersionEntityObject } from '@app-models/entities/i-version-entity-object';
+import { VersionEntityObjectBaseListComponent }from '@app/components/entities/version-entity-object-base-list.component';
+import { VersionEntityObjectEditComponent }from '@app/components/entities/version-entity-object-edit.component';
+import { VersionEntityObjectService } from '@app-services/http/entities/version-entity-object-service';
+//@CustomImportBegin
+//@CustomImportEnd
+@Component({
+  standalone: true,
+  selector:'app-version-entity-object-list',
+  imports: [ CommonModule, FormsModule, TranslateModule, RouterModule ],
+  templateUrl: './version-entity-object-list.component.html',
+  styleUrl: './version-entity-object-list.component.css'
+})
+export class VersionEntityObjectListComponent extends VersionEntityObjectBaseListComponent {
+  constructor(protected override dataAccessService: VersionEntityObjectService)
+  {
+    super(dataAccessService);
+  }
+  override ngOnInit(): void {
+    this._queryParams.filter = '';
+    this.reloadData();
+  }
+  protected override getItemKey(item: IVersionEntityObject): IdType {
+    return item?.id || IdDefault;
+  }
+  override get pageTitle(): string {
+    return 'VersionEntityObjects';
+  }
+  override getEditComponent() {
+    return VersionEntityObjectEditComponent;
+  }
+//@CustomCodeBegin
+//@CustomCodeEnd
+}
